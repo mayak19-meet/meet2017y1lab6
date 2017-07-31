0a8fbe745e5fae635fa2f9b405eafd74e54fabed
mayak19-meet <mayak19@meet.mit.edu>
2017-07-31 17:21:24 +0300

    Stopping here

---
+++ funturtle.py
@@ -0,0 +1,36 @@
+import turtle
+##turtle.shape('turtle')
+##square=turtle.clone()
+##square.shape('square')
+##square.goto(100,100)
+##square.goto(300,300)
+##square.stamp()
+##square.goto(100,100)
+##square.penup()
+##square.goto(0,0)
+##square.pendown()
+##square.goto(0,100)
+##square.goto(100,100)
+##square.goto(100,0)
+##square.goto(0,0)
+##square=turtle.clone()
+##square.shape('triangle')
+##square.goto(100,100)
+##square.goto(0,200)
+##square.goto(0,0)
+##square=turtle.clone()
+##square.shape('triangle')
+##square=turtle.clone()
+up_arrow="up"
+leet_arrow="left"
+DOWN_arrow="down"
+right_arrow="right"
+spacebar="space"
+
+
+up=0
+direction=up
+
+def up()
+gl
+#tutrle.mineloop()
